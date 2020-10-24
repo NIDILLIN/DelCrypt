992f472869fe604baa3e39937ab887fc3a37888f
NiDillin <nidillin@gmail.com>
2020-10-24 16:29:34 +0300

    nothing to remand

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
 # This Python file uses the following encoding: utf-8
 
 import sys  # sys нужен для передачи argv в QApplication
-from PyQt5 import QtWidgets
+import find_ext as fext 
+
+
+finder = fext.find_ext()
+path = os.path(__file__)
+
+extension = finder.split_filename_and_find_ext_in(path)
+
+analyzer = ext_analyzer()
+
+analyzer.analyze(extension)
