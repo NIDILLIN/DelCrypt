97c3996d37ae6401ef3c5209aca13cd2aff1ed10
NiDillin <nidillin@gmail.com>
2020-10-24 16:31:16 +0300

    Make tries to fill the defs

--- find_ext.py
+++ find_ext.py
@@ -2,19 +2,34 @@
 import os
 
 class find_ext():
-	def __init__(self):
-		pass
+	def split_and_find_ext_in(self, p):
+		self._path = p
+		self._path = os.path.(__file__)
 
-	def find(self):
-		pass
+		self._filename, self._file_ext = os.path.splitext(self._path)
+
+		return self._filename, self._file_ext
+
+class ext_analyzer(find_ext):
+	if self._file_ext = ""
 
 
 
+path = '/users/nidillin/Desktop/file_Name.ba6'
 
+finder = find_ext()
+finder.split(path) #returned file_Name & .ba6
+
+
+.ba6
+.a6
+.b64
+.sha
 
 """
 import os
 
 filename, file_extension = os.path.splitext('/path/to/somefile.ext')
+that means split extension of file 
 
 """
