e84c3fae821b0b81ce17494603187b250dc51840
NiDillin <nidillin@gmail.com>
2020-10-24 06:53:58 +0300

    Turn name from find_end to find_ext

---
+++ find_ext.py
@@ -0,0 +1,20 @@
+#-*-coding: utf-8-*-
+import os
+
+class find_ext():
+	def __init__(self):
+		pass
+
+	def find(self):
+		pass
+
+
+
+
+
+"""
+import os
+
+filename, file_extension = os.path.splitext('/path/to/somefile.ext')
+
+"""
