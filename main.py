934c1c764213cc28bad74f3a1ebf4f89da14212e
NiDillin <nidillin@gmail.com>
2020-10-25 16:07:46 +0300

    Code will be able to find "Modules" folder. objects $find_ext$ could keep path to file in _path. Ext_analyzer can tell you what is ext is using

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
 #Copyright 2020 Nico Dillinger
@@ -20,17 +20,22 @@ along with DelCrypt.  If not, see <https://www.gnu.org/licenses/>.
 """
 
 # import gui
-# sys.path.append('/Users/nidillin/DelCrypt/Modules')
-# import ext_tools as fext
+import sys, os
+sys.path.append('./Modules')
 
-# finder = fext.find_ext()
-# path = os.path(__file__)
+import ext_tools as fext
 
-# extension = finder.split_filename_and_find_ext_in(path)
+finder = fext.find_ext()
 
-# analyzer = ext_analyzer()
+path = "/Modules/ext_tools.py"
+file_path, extension = finder.split_filename_in(path)
+print(finder._path)
 
-# analyzer.analyze(extension)
+print(extension)
+print(file_path)
+
+analyzer = fext.ext_analyzer()
+analyzer.analyze(extension)
 
 
 
